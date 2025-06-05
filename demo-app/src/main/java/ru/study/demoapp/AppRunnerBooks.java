132514aebf5922b942f981e97ab1829e927eb989
Oleg Klimkin <oleg402@bk.ru>
2025-06-05 23:24:23 +0300

    added caching;

--- demo-app/src/main/java/ru/study/demoapp/AppRunnerBooks.java
+++ demo-app/src/main/java/ru/study/demoapp/AppRunnerBooks.java
@@ -19,12 +19,12 @@ public class AppRunnerBooks implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-/*        logger.info(".... Fetching books");
+        logger.info(".... Fetching books");
         logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
         logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
         logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
         logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
         logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
-        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));*/
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
     }
 }
