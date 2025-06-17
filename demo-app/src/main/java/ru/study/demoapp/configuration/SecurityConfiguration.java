629a70ac0833def35f4195c41250343ee449227e
Oleg Klimkin <oleg402@bk.ru>
2025-06-17 16:23:48 +0300

    added basic auth

---
+++ demo-app/src/main/java/ru/study/demoapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package ru.study.demoapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails vasya = User.builder()
+                .username("vasya")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails petya = User.builder()
+                .username("petya")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(vasya, petya);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/add-user").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
--- demo-app/src/main/java/ru/study/demoapp/controller/MyController.java
+++ demo-app/src/main/java/ru/study/demoapp/controller/MyController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.study.demoapp.metrics.CounterMetric;
 
 import java.util.Locale;
+import java.util.Map;
 
 @RestController
 public class MyController {
@@ -30,5 +33,20 @@ public class MyController {
     @GetMapping("/hello-lang")
     public String getGreeting(Locale locale) {
         return messageSource.getMessage("greeting", null, locale);
+    }
+
+    @GetMapping("/user/info")
+    public String userInfo() {
+        return "Hello, user!";
+    }
+
+    @GetMapping("/admin/info")
+    public String adminInfo() {
+        return "Hello, admin!";
+    }
+
+    @PostMapping("/add-user")
+    public String addUser(@RequestBody Map<String, String> user) {
+        return "Условно добавили юзера " + user.get("username") + " с паролем " + user.get("password");
     }
 }
